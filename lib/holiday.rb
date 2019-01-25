bb135fe0d7e520e91370d4dbed640a85851ac4ad
Oliver <oliver.grant@gmail.com>
2019-01-25 11:55:27 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -46,7 +46,6 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-  binding.pry
   return holiday_hash[:winter].values.flatten
 end
 
